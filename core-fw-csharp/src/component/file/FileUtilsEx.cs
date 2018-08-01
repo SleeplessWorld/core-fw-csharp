92ed1f7a679e40b0b7354dc689d34b2bcf8b0e06
JHercules <JHercules_qz@qq.com>
2018-08-01 18:11:40 +0800

    update Project

---
+++ core-fw-csharp/src/component/file/FileUtilsEx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace walle.component.file
+{
+    public class FileUtilsEx
+    {
+        #region openFolder
+
+        public static void openFolder(String strFolderPath)
+        {
+            if (Directory.Exists(strFolderPath))
+            {
+                Process.Start("explorer", strFolderPath);
+            }
+        }
+
+        #endregion
+
+        #region findFile
+
+        public static String findFile(String strFolderPath, String strFileName)
+        {
+            String strRes = "";
+
+            List<String> lstFilePath = Directory.GetFiles(strFolderPath).ToList();
+
+            foreach (var strFilePath in lstFilePath)
+            {
+                if (strFilePath.Contains(strFileName))
+                {
+                    strRes = strFilePath;
+                    break;
+                }
+            }
+
+            return strRes;
+        }
+
+        #endregion
+
+        #region exists
+
+        public static bool exists(String strFilePath)
+        {
+            return File.Exists(strFilePath);
+        }
+
+        #endregion
+
+        #region folderExists
+
+        public static bool folderExists(String strFolderPath)
+        {
+            return Directory.Exists(strFolderPath);
+        }
+
+        #endregion
+    }
+}
---
+++ core-fw-csharp/src/component/json/JsonUtilsEx.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace walle.component.json
+{
+    public class JsonUtilsEx
+    {
+        #region toStr
+
+        public static String toStr(Object oObj)
+        {
+            String strRes = JsonConvert.SerializeObject(oObj);
+            return strRes;
+        }
+
+        #endregion
+
+        #region toObj
+
+        #endregion
+    }
+}
